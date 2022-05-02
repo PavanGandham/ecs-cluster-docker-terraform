b9281845a8f9fd93d1137d4fff5c35268fe9455b
Swain Dennis <52460209+Soulsounds@users.noreply.github.com>
2022-05-02 14:02:42 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "vpc_cidr" {
+  description = "CIDR range of VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
